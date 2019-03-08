398689cccf3a36fc915967caf1029740a84eaf95
ces1508 <ces1508@gmail.com>
2019-03-08 17:17:30 -0500

    set views

--- src/components/shared/Drawer.vue
+++ src/components/shared/Drawer.vue
@@ -1,29 +1,41 @@
 <template>
-  <v-navigation-drawer v-model="showDrawer">
+  <v-navigation-drawer v-model="drawer" :mobile-break-point="1024" clipped persistent app>
       <v-list dense>
-        <v-list-tile>
+        <v-list-tile v-for="page in pages" :key="page.title" @click="$router.push(page.path)">
           <v-list-tile-action>
-            <v-icon>home</v-icon>
+            <v-icon>{{ page.icon }}</v-icon>
           </v-list-tile-action>
           <v-list-tile-content>
-            <v-list-tile-title>Home</v-list-tile-title>
-          </v-list-tile-content>
-        </v-list-tile>
-        <v-list-tile >
-          <v-list-tile-action>
-            <v-icon>contact_mail</v-icon>
-          </v-list-tile-action>
-          <v-list-tile-content>
-            <v-list-tile-title>Contact</v-list-tile-title>
+            <v-list-tile-title>{{ page.title }}</v-list-tile-title>
           </v-list-tile-content>
         </v-list-tile>
       </v-list>
     </v-navigation-drawer>
 </template>
 <script>
+import firebase from 'firebase'
 export default {
   props: {
-    showDrawer: Boolean
+    drawer: Boolean
+  },
+  data () {
+    return {
+      pages: [
+        { title: 'Home', path: '/home', icon: 'home' },
+        { title: 'Completed', path: '/completed', icon: 'playlist-check' },
+        { title: 'Cuenta', path: '/account', icon: 'person' }
+      ]
+    }
+  },
+  methods: {
+    async logout () {
+      try {
+        await firebase.auth().signOut()
+        this.$router.replace('/login')
+      } catch (e) {
+        alert('Lo sentimos, estamos presentendo problemas para desconectarte')
+      }
+    }
   }
 }
 </script>
---
+++ src/views/Account.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <header-bar></header-bar>
+    <v-content>
+      <v-container>
+        <v-layout>
+          <v-flex>
+            <v-form xs12 sm8 ofset-sm-2 md6 ofset-md3>
+              <v-text-field v-model="profile.firstName" label="nombres"/>
+              <v-text-field v-model="profile.lastName" label="apellidos"/>
+              <v-text-field v-model="profile.address" label="dirrecion"/>
+              <v-text-field v-model="profile.phone" label="telefono"/>
+              <v-btn color='success'>Actualizar perfil</v-btn>
+            </v-form>
+          </v-flex>
+        </v-layout>
+      </v-container>
+    </v-content>
+  </div>
+</template>
+<script>
+import Header from '@/components/shared/Header'
+export default {
+  components: {
+    'header-bar': Header
+  },
+  data () {
+    return {
+      profile: {
+        firstName: 'chrsitian',
+        lastName: 'Segura',
+        address: 'carrera 30 A # 2 i 23',
+        phone: '3203230522'
+      }
+    }
+  }
+}
+</script>
